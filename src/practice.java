f088cddc97fa4ec1705ef078333841987e7c6de9
lucyoz <lucyoz.kwon@gmail.com>
2022-10-05 09:17:04 +0900

    first commit 1. a와 b의 값을 swap 로직

---
+++ src/practice.java
@@ -0,0 +1,10 @@
+public class practice {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+
+        int temp = b;
+        b = a;
+        a = temp;
+    }
+}
